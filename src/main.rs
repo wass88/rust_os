f5f84c9f8032ad821831cd44664591727fca15c9
wass80 <wasss80@gmail.com>
2022-04-14 17:42:32 +0900

    boot loop!

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,9 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
