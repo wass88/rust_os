c204c8c6beb59dbf18aeaf0d80ab343d51aa51b0
wass80 <wasss80@gmail.com>
2022-04-15 21:33:09 +0900

    Empty Alllocate

--- src/main.rs
+++ src/main.rs
@@ -14,31 +14,22 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use rust_os::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{structures::paging::Page, VirtAddr};
 
     println!("Hello World{}", "!");
     rust_os::init();
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // 追加：mapperを初期化
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        // 恒等対応しているVGAバッファのページ
-        0xb8000,
-        // コードページのどこか
-        0x201008,
-        // スタックページのどこか
-        0x0100_0020_1a10,
-        // 物理アドレス "0" にマップされている仮想アドレス
-        boot_info.physical_memory_offset,
-    ];
+    // たぶん未使用のアドレスにVGAにマップする
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        // 追加：`mapper.translate_addr`メソッドを使う
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // 新しいマッピングを使って、文字列`New!`を画面に書き出す
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
