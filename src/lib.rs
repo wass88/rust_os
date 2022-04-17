fd18038a03d8ec57b705659c802570914092fb91
wass80 <wasss80@gmail.com>
2022-04-17 19:37:53 +0900

    dummy allocator

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
 
 extern crate alloc;
 
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
@@ -92,4 +94,9 @@ pub fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,31 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
 use rust_os::println;
 
-use bootloader::{entry_point, BootInfo};
 use x86_64::VirtAddr;
+
+use bootloader::{entry_point, BootInfo};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory;
-    use x86_64::{structures::paging::Page, VirtAddr};
-
     println!("Hello World{}", "!");
     rust_os::init();
 
+    use rust_os::memory::{self, BootInfoFrameAllocator};
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator =
-        unsafe { rust_os::memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    rust_os::allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap init failed");
+
+    use alloc::boxed::Box;
+    let x = Box::new(41);
 
     #[cfg(test)]
     test_main();
